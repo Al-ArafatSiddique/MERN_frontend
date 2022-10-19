5b7cf238b24acbbbb98ee3df29f614a1536df2fc
Al Arafat Siddique <arafatdipto@gmail.com>
2022-10-19 12:47:21 +0600

    Folder structure, React Router Dom implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NewPlace from './places/pages/NewPlace';
+import User from './user/pages/User';
 
 
 function App() {
   return (
-    <div className="App">
-     Hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<User/>} />
+        </Route>
+        <Route path='/places'>
+          <Route path='new' element={<NewPlace/>} />
+        </Route>
+        <Route path='*' element={<h1>Not Found</h1>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
