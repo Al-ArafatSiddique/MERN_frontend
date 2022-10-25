365a952ced45b58c9e571460074dc555c3cb96c7
Al Arafat Siddique <arafatdipto@gmail.com>
2022-10-25 17:21:17 +0600

    Showing own places for users- Using useParams hook

--- src/places/pages/UserPlaces.jsx
+++ src/places/pages/UserPlaces.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import PlaceList from '../components/PlaceList'
 
 const DUMMY_PLACES=[{
@@ -26,7 +27,9 @@ const DUMMY_PLACES=[{
   creator: 'u2'
 }]
 export default function UserPlaces() {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place=>place.creator === userId);
   return (
-    <PlaceList items={DUMMY_PLACES} />
+    <PlaceList items={loadedPlaces} />
   )
 }
